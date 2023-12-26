10463bfaa7f6bff5457bcbce474117e16c04cb7f
owais34 <owaishunter78@gmail.com>
2023-12-26 16:57:30 +0530

    Added pipeline scripts

---
+++ pipeline.ps1
@@ -0,0 +1,5 @@
+python3 -m venv venv;
+.\venv\Scripts\Activate.ps1;
+pip install -r requirements.txt;
+pytest;
+
---
+++ pipeline.py
@@ -0,0 +1,53 @@
+import subprocess
+import sys
+
+command_option = sys.argv[1]
+
+# def createVirtualEnvironment():
+#     try:
+#         subprocess.run("python3 -m venv venv")
+#     except Exception as e:
+#         print(e)
+#         print("trying with python version < 3")
+#         try:
+#             subprocess.run("python -m venv venv")
+#             return True
+#         except Exception as e2:
+#             print(e2)
+#             return False
+#     return True
+
+# def activateVirtualEnvironment():
+#     try:
+#         if sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
+#             subprocess.run("source env/bin/activate")
+#         elif sys.platform.startswith("win32"):
+#             subprocess.run(".\venv\Scripts\Activate.ps1")
+#     except Exception as e:
+#         print(e)
+#         return False
+#     return True
+
+
+def installDependencies():
+    subprocess.run("pip install -r requirements.txt")
+    return True
+
+
+def runTests():
+    subprocess.run("pytest")
+    return True
+
+
+
+if command_option == "install":
+    result = installDependencies() and runTests()
+elif command_option == "test":
+    runTests()
+else:
+    print("please use option install or test >> py pipeline.py install or py pieline.py test")
+    
+            
+
+
+
