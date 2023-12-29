bffdabc2d0f3540082a43f21be4eb5a8291a5fec
owais34 <owaishunter78@gmail.com>
2023-12-29 23:48:32 +0530

    Fixed bulk string , made singleton class , refactored tests

--- src/resp/deserializer.py
+++ src/resp/deserializer.py
@@ -1,4 +1,5 @@
 from collections import deque
+from src.utils.util_classes import singleton
 
 # implement serializer and deserializer compatible for both RESP2 & RESP3
 
@@ -22,139 +23,145 @@ from collections import deque
 # 17)Sets -> ~<number-of-elements>\r\n<element-1>...<element-n> , 
 
 BULK_STRING_LIMIT = 512000000
-SIMPLE_STRING_LIMIT = 100
+SIMPLE_STRING_LIMIT = 100000
 
-def deserialize(serialized_input: str):
-    # Implement this function
-    # parse above string and return a python data type as output ( it should be a one-to-one translation )
-    # Example : +OK\r\n => "OK" , 
-    #  *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n  -> ["hello","world"]
-    # ["*2","$5","hello","$5","world"]
-    token_queue = deque(serialized_input.split("\r\n"))
-    return deserialize_helper(token_queue)
+@singleton
+class Deserializer:
 
-def deserialize_helper(token_queue: deque):
-    current_element = token_queue.popleft()
-    
-    match current_element[0]:
-        case "+":
-            # handle simple string
-            if len(current_element[1:])>SIMPLE_STRING_LIMIT:
-                raise Exception("Max string length exceeded for Simple String")
-            return current_element[1:]
-        case "-":
-            # Error handling to be implemented below placeholder for now
-            exception = Exception(current_element[1:])
-            return exception
-        case ":":
-            return get_int(current_element)
-        case "$":
-            string_length = int(current_element[1:])
-            if string_length>BULK_STRING_LIMIT:
-                raise Exception("Max string length exceeded for Bulk String")
-            return get_bulk_string(token_queue, string_length)
-        case "*":
-            array_length = int(current_element[1:])
-            return get_array(token_queue,array_length)
-        case "_":
-            return None
-        case "#":
-            return get_boolean(current_element)
-        case ",":
-            return get_double(current_element)
-        case "(":
-            return get_int(current_element)
-        case "!":
-            string_length = int(current_element[1:])
-            return get_bulk_error(token_queue,string_length)
-        case "=":
-            string_length = int(current_element[1:])
-            return get_verbatim_string(token_queue,string_length)
-        case "%":
-            map_length = int(current_element[1:])
-            return get_map(token_queue, map_length=map_length)
-        case "~":
-            set_length = int(current_element[1:])
-            return get_set(token_queue, set_length=set_length)
+
+    def deserialize(self, serialized_input: str):
+        # Implement this function
+        # parse above string and return a python data type as output ( it should be a one-to-one translation )
+        # Example : +OK\r\n => "OK" , 
+        #  *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n  -> ["hello","world"]
+        # ["*2","$5","hello","$5","world"]
+        token_queue = deque(serialized_input.split("\r\n"))
+        return self.deserialize_helper(token_queue)
+
+    def deserialize_helper(self, token_queue: deque):
+        current_element = token_queue.popleft()
         
-        
+        match current_element[0]:
+            case "+":
+                # handle simple string
+                if len(current_element[1:])>SIMPLE_STRING_LIMIT:
+                    raise Exception("Max string length exceeded for Simple String")
+                elif current_element[1:].find("\r") != -1 or current_element[1:].find("\n")!= -1:
+                    raise Exception("Simple string should not have escape sequences")
+                return current_element[1:]
+            case "-":
+                # Error handling to be implemented below placeholder for now
+                exception = Exception(current_element[1:])
+                return exception
+            case ":":
+                return self.get_int(current_element)
+            case "$":
+                string_length = int(current_element[1:])
+                if string_length>BULK_STRING_LIMIT:
+                    raise Exception("Max string length exceeded for Bulk String")
+                return self.get_bulk_string(token_queue, string_length)
+            case "*":
+                array_length = int(current_element[1:])
+                return self.get_array(token_queue,array_length)
+            case "_":
+                return None
+            case "#":
+                return self.get_boolean(current_element)
+            case ",":
+                return self.get_double(current_element)
+            case "(":
+                return self.get_int(current_element)
+            case "!":
+                string_length = int(current_element[1:])
+                return self.get_bulk_error(token_queue,string_length)
+            case "=":
+                string_length = int(current_element[1:])
+                return self.get_verbatim_string(token_queue,string_length)
+            case "%":
+                map_length = int(current_element[1:])
+                return self.get_map(token_queue, map_length=map_length)
+            case "~":
+                set_length = int(current_element[1:])
+                return self.get_set(token_queue, set_length=set_length)
             
+            
+                
 
-def get_int(current_element: str) -> int:
-    return int(current_element[1:])
+    def get_int(self, current_element: str) -> int:
+        return int(current_element[1:])
 
-def get_bulk_string(token_queue: deque, string_length: int) -> str:
-    if string_length == -1:
-        return None
-    
-    bulk_string = token_queue.popleft()
-
-    if len(bulk_string) != string_length:
-        raise Exception("ERROR bulk string length doesnt match")
-    return bulk_string
-
-def get_array(token_queue: deque, array_length: int) -> list:
-    if array_length == -1:
-        return None
-    array = []
-    for iterator in range(array_length):
-        array.append(deserialize_helper(token_queue))
-        # Add better error handling
-        # except Exception as e:
-        #     if str(e).startswith("IndexError: pop from an empty deque"):
-        #         raise Exception("ERROR while parsing array , expected "+array_length+" found "+iterator)
-        #     else:
-        #         raise e
-    return array
+    def get_bulk_string(self, token_queue: deque, string_length: int) -> str:
+        if string_length == -1:
+            return None
         
-def get_boolean(current_element: str)->bool:
-    if current_element[1:] == "t":
-        return True
-    elif current_element[1:] == "f":
-        return False
-    else:
-        raise Exception("Invalid boolean type expected t/f found " + current_element[1:])
+        bulk_string = token_queue.popleft()
+        byte_array_len = len(bytes(bulk_string,"utf-8"))
+        if byte_array_len != string_length:
+            raise Exception("ERROR bulk string length doesnt match")
+        return bulk_string
 
-def get_double(current_element: str) -> float:
-    return float(current_element[1:])
+    def get_array(self, token_queue: deque, array_length: int) -> list:
+        if array_length == -1:
+            return None
+        array = []
+        for iterator in range(array_length):
+            array.append(self.deserialize_helper(token_queue))
+            # Add better error handling
+            # except Exception as e:
+            #     if str(e).startswith("IndexError: pop from an empty deque"):
+            #         raise Exception("ERROR while parsing array , expected "+array_length+" found "+iterator)
+            #     else:
+            #         raise e
+        return array
+            
+    def get_boolean(self, current_element: str)->bool:
+        if current_element[1:] == "t":
+            return True
+        elif current_element[1:] == "f":
+            return False
+        else:
+            raise Exception("Invalid boolean type expected t/f found " + current_element[1:])
 
-def get_bulk_error(token_queue: deque, string_length: int) -> str:
-    if string_length == -1:
-        return None
-    
-    bulk_string = token_queue.popleft()
+    def get_double(self, current_element: str) -> float:
+        return float(current_element[1:])
 
-    if len(bulk_string) != string_length:
-        raise Exception("ERROR bulk string length doesnt match")
-    return Exception(bulk_string)
+    def get_bulk_error(self, token_queue: deque, string_length: int) -> str:
+        if string_length == -1:
+            return None
+        
+        bulk_string = token_queue.popleft()
 
-def get_verbatim_string(token_queue: deque, string_length: int) -> dict:
-    raw_string = get_bulk_string(token_queue,string_length)
-    components = raw_string.split(":")
-    file_format = components[0]
-    output_dict = {}
-    output_dict["format"]=file_format
-    output_dict["bytes"]=bytes(components[1], "utf-8")
-    return output_dict
+        if len(bulk_string) != string_length:
+            raise Exception("ERROR bulk string length doesnt match")
+        return Exception(bulk_string)
 
-def get_map(token_queue: deque, map_length: int) -> dict:
-    output_dictionary = {}
+    def get_verbatim_string(self, token_queue: deque, string_length: int) -> dict:
+        raw_string = self.get_bulk_string(token_queue,string_length)
+        components = raw_string.split(":")
+        file_format = components[0]
+        output_dict = {}
+        output_dict["format"]=file_format
+        output_dict["bytes"]=bytes(components[1], "utf-8")
+        return output_dict
 
-    for iterator in range(map_length):
-        current_key = deserialize_helper(token_queue)
-        if isinstance(current_key,str) == False:
-            raise Exception("Invalid Key Type expected str found "+str(current_key.__class__))
-        current_value = deserialize_helper(token_queue)
-        output_dictionary[current_key]=current_value
+    def get_map(self, token_queue: deque, map_length: int) -> dict:
+        output_dictionary = {}
 
-    return output_dictionary
+        for iterator in range(map_length):
+            current_key = self.deserialize_helper(token_queue)
+            if isinstance(current_key,str) == False:
+                raise Exception("Invalid Key Type expected str found "+str(current_key.__class__))
+            current_value = self.deserialize_helper(token_queue)
+            output_dictionary[current_key]=current_value
 
-def get_set(token_queue: deque, set_length: int) -> set:
-    output_set = set()
-    for iterator in range(set_length):
-        output_set.add(deserialize_helper(token_queue))
+        return output_dictionary
 
-    return output_set
+    def get_set(self, token_queue: deque, set_length: int) -> set:
+        output_set = set()
+        for iterator in range(set_length):
+            output_set.add(self.deserialize_helper(token_queue))
+
+        return output_set
 
 
 # print(deserialize("%2\r\n+first\r\n%2\r\n+nestedfirst\r\n:3\r\n+nestedSecond\r\n:4\r\n+second\r\n:2\r\n"))
--- src/resp/serializer.py
+++ src/resp/serializer.py
@@ -1,78 +1,115 @@
 from sys import getsizeof
+from src.utils.util_classes import singleton
 
 BULK_STRING_LIMIT = 512000000
 SIMPLE_STRING_LIMIT = 10
 
 DELIMITER = "\r\n"
 
-def serialize(deserialized_input: any) -> str:
-    if deserialized_input == None:
-        return "_"+DELIMITER
-    elif isinstance(deserialized_input, str):
-        return encode_string(deserialized_input)
-    elif isinstance(deserialized_input, Exception):
-        return encode_exception(deserialized_input)
-    elif isinstance(deserialized_input, int):
-        return encode_int(deserialized_input)
-    elif isinstance(deserialized_input, list):
-        return encode_list(deserialized_input)
-    elif isinstance(deserialized_input, bool):
-        return encode_boolean(deserialized_input)
-    elif isinstance(deserialized_input, float):
-        return encode_float(deserialized_input)
+@singleton
+class Serializer:
+
+    def serialize(self, deserialized_input: any) -> str:
+        if deserialized_input == None:
+            return "_"+DELIMITER
+        elif isinstance(deserialized_input, str):
+            return self.encode_string(deserialized_input)
+        elif isinstance(deserialized_input, Exception):
+            return self.encode_exception(deserialized_input)
+        elif isinstance(deserialized_input, bool):
+            return self.encode_boolean(deserialized_input)
+        elif isinstance(deserialized_input, int):
+            return self.encode_int(deserialized_input)
+        elif isinstance(deserialized_input, list):
+            return self.encode_list(deserialized_input)
+        elif isinstance(deserialized_input, float):
+            return self.encode_float(deserialized_input)
+        elif self.is_verbatim_string(deserialized_input):
+            return self.encode_bytes(deserialized_input)
+        elif isinstance(deserialized_input, dict):
+            return self.encode_map(deserialized_input)
+        elif isinstance(deserialized_input, set):
+            return self.encode_set(deserialized_input)
+        
     
+    def is_verbatim_string(self, deserialized_input) -> bool:
+        return isinstance(deserialized_input, dict) and \
+            "bytes" in deserialized_input and \
+            "format" in deserialized_input and \
+            len(deserialized_input) == 2 and \
+            isinstance(deserialized_input.get("bytes"),bytes) 
+            
 
-def encode_string(deserialized_input: str) -> str:
-    if len(deserialized_input)<=SIMPLE_STRING_LIMIT:
-        return "+"+deserialized_input+DELIMITER
-    else:
-         return encode_bulk_string(deserialized_input)
+    def encode_string(self, deserialized_input: str) -> str:
+        if len(deserialized_input)<=SIMPLE_STRING_LIMIT:
+            return "+"+deserialized_input+DELIMITER
+        else:
+            return self.encode_bulk_string(deserialized_input)
 
-def encode_bulk_string(deserialized_input: str) -> str:
-    if getsizeof(deserialized_input)<=BULK_STRING_LIMIT:
-        string_len = "$"+str(len(deserialized_input))
-        components = [string_len,deserialized_input,""]
-        return DELIMITER.join(components)
-    else:
-         raise Exception("String length exceeded")
+    def encode_bulk_string(self, deserialized_input: str) -> str:
+        byte_array_len = len(bytes(deserialized_input,"utf-8"))
+        if byte_array_len<=BULK_STRING_LIMIT:
+            string_len = "$"+str(byte_array_len)
+            components = [string_len,deserialized_input,""]
+            return DELIMITER.join(components)
+        else:
+            raise Exception("max length exceeded")
 
-def encode_exception(deserialized_input: Exception) -> str:
-    error_len = len(str(deserialized_input))
-    if error_len<=10:
-        return "-"+str(deserialized_input)+DELIMITER
-    else:
-        components = ["!"+str(error_len),str(deserialized_input),""]
-        return DELIMITER.join(components)
+    def encode_exception(self, deserialized_input: Exception) -> str:
+        error_len = len(str(deserialized_input))
+        if error_len<=10:
+            return "-"+str(deserialized_input)+DELIMITER
+        else:
+            components = ["!"+str(error_len),str(deserialized_input),""]
+            return DELIMITER.join(components)
 
 
-def encode_int(deserialized_input: int) -> str:
-    if getsizeof(deserialized_input) <= 64:
-        return ":"+str(deserialized_input)+DELIMITER
-    else:
-        return "("+str(deserialized_input)+DELIMITER
+    def encode_int(self, deserialized_input: int) -> str:
+        if getsizeof(deserialized_input) <= getsizeof(0):
+            return ":"+str(deserialized_input)+DELIMITER
+        else:
+            return "("+str(deserialized_input)+DELIMITER
 
-def encode_list(deserialized_input: list) -> str:
-    components = []
-    list_len = len(deserialized_input)
-    components.append("*"+str(list_len)+DELIMITER)
-    for iterator in range(list_len):
-        components.append(serialize(deserialized_input[iterator]))
+    def encode_list(self, deserialized_input: list) -> str:
+        components = []
+        list_len = len(deserialized_input)
+        components.append("*"+str(list_len)+DELIMITER)
+        for iterator in range(list_len):
+            components.append(self.serialize(deserialized_input[iterator]))
+        
+        return "".join(components)
+
+    def encode_boolean(self, deserialized_input: bool) -> str:
+        if deserialized_input:
+            return "#t"+DELIMITER
+        else:
+            return "#f"+DELIMITER
+        
+    def encode_float(self, deserialized_input: float) -> str:
+        return ","+str(deserialized_input)+DELIMITER
+
+    def encode_bytes(self, deserialized_input: dict) -> str:
+        component_2 = deserialized_input["format"]+":"+deserialized_input["bytes"].decode("utf-8")
+        component_1 = "="+str(len(bytes(component_2,"utf-8")))
+        return DELIMITER.join([component_1,component_2,""])
     
-    return "".join(components)
-
-def encode_boolean(deserialized_input: bool) -> str:
-    if deserialized_input:
-        return "#t"+DELIMITER
-    else:
-        return "#f"+DELIMITER
+    def encode_map(self, deserialized_input: dict) -> str:
+        map_length = len(deserialized_input)
+        components = ["%"+str(map_length)+DELIMITER]
+        for key in deserialized_input:
+            components.append(self.encode_string(key))
+            components.append(self.serialize(deserialized_input.get(key)))
+        
+        return "".join(components)
     
-def encode_float(deserialized_input: float) -> str:
-    return ","+str(deserialized_input)+DELIMITER
+    def encode_set(self, deserialized_input: set) -> str:
+        set_length = len(deserialized_input)
+        components = ["~"+str(set_length)+DELIMITER]
+        for element in deserialized_input:
+            components.append(self.serialize(element))
+        
+        return "".join(components)
 
-def encode_bytes(deserialized_input: dict) -> str:
-    component_2 = deserialized_input["format"]+":"+deserialized_input["bytes"].decode("utf-8")
-    component_1 = "="+str(len(bytes(component_2,"utf-8")))
-    return DELIMITER.join([component_1,component_2,""])
 
 
 
